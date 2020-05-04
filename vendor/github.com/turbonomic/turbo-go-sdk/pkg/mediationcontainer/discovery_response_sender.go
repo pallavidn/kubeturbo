ceab284c36488b4c05606f2d0cf218a406002803
pallavi_debnath <pallavi.debnath@turbonomic.com>
2020-05-04 10:48:47 -0400

    local vendor changes 5/1

--- vendor/github.com/turbonomic/turbo-go-sdk/pkg/mediationcontainer/discovery_response_sender.go
+++ vendor/github.com/turbonomic/turbo-go-sdk/pkg/mediationcontainer/discovery_response_sender.go
@@ -1,7 +1,6 @@
 package mediationcontainer
 
 import (
-	"github.com/golang/glog"
 	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
 )
 
@@ -31,32 +30,6 @@ func (d *DiscoveryResponseSender) Send(discoveryResponse *proto.DiscoveryRespons
 // will be generated and sent. This is required to get the DTOs passed at server side.
 func (d *DiscoveryResponseSender) chunkDiscoveryResponse(dr *proto.DiscoveryResponse) []*proto.DiscoveryResponse {
 	chunks := []*proto.DiscoveryResponse{}
-	//ver := "version1"
-	//chunk := &proto.DiscoveryResponse{
-	//	PriceTable: &proto.PriceTable{
-	//		Version:                   &ver,
-	//		PriceTableKeys:             nil,
-	//		OnDemandPriceTable:         nil,
-	//		SpotPriceTable:             nil,
-	//		ReservedInstancePriceTable: nil,
-	//		OnDemandLicensePriceTable:  nil,
-	//		ReservedLicensePriceTable:  nil,
-	//		ServiceProviderId:          nil,
-	//		XXX_unrecognized:           nil,
-	//	},
-	//}
-	//
-	//chunks = append(chunks, chunk)
-
-	if dr.DiscActionSpec != nil {
-		glog.Infof("Adding DiscActionSpec to the response ... %++v", dr.DiscActionSpec)
-		chunk := &proto.DiscoveryResponse{
-			DiscActionSpec: &proto.DiscoveredActionSpec{
-				Specs:            dr.DiscActionSpec.Specs,
-			},
-		}
-		chunks = append(chunks, chunk)
-	}
 
 	if len(dr.EntityDTO) > 0 {
 		chunk := &proto.DiscoveryResponse{
@@ -124,6 +97,13 @@ func (d *DiscoveryResponseSender) chunkDiscoveryResponse(dr *proto.DiscoveryResp
 	if len(dr.FlowDTO) > 0 {
 		chunk := &proto.DiscoveryResponse{
 			FlowDTO: dr.FlowDTO,
+		}
+		chunks = append(chunks, chunk)
+	}
+
+	if len(dr.ActionMergeSpecs) > 0 {
+		chunk := &proto.DiscoveryResponse{
+			ActionMergeSpecs: dr.ActionMergeSpecs,
 		}
 		chunks = append(chunks, chunk)
 	}
